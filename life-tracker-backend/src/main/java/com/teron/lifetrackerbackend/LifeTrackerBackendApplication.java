b2866c34337d036bde4a7dfef5440fb17785f7d3
ter0n <tyarin.petr@gmail.com>
2024-02-20 21:50:54 +0300

    Add backend
    Add backend structure

---
+++ life-tracker-backend/src/main/java/com/teron/lifetrackerbackend/LifeTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teron.lifetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ life-tracker-backend/src/test/java/com/teron/lifetrackerbackend/LifeTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teron.lifetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
